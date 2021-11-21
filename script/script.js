91d7c44af257fc343951d263d854a2a06da65feb
OrislavaM <orisyamatvienko@gmail.com>
2021-11-21 23:59:21 +0200

    SPA logic, basic html and css-styles

---
+++ script/script.js
@@ -0,0 +1,35 @@
+// SPA logic
+
+window.frames[0].location.href = "./html/today.html";
+
+let active_block = window.menu.children;
+active_block[0].classList.add("active");
+
+const menu = document.getElementById("menu");
+
+menu.addEventListener("click", (e) => {
+    Array.from(active_block).forEach((e) => {
+        if (e.classList.contains("active")) {
+            e.classList.remove("active");
+        }
+    });
+
+    switch (e.target.dataset.menu) {
+        case "today":
+            {
+                window.frames[0].location.href = "./html/today.html";
+                document.title = "today";
+                e.target.classList.add("active");
+            }
+            break;
+        case "five_days":
+            {
+                window.frames[0].location.href = "./html/five_days.html";
+                document.title = "five_days";
+                e.target.classList.add("active");
+            }
+            break;
+        default:
+            console.log("no even menu");
+    }
+});
